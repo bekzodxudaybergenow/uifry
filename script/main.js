c5ae882529ca4e50d96b7917c901a4bdc362061c
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-04-02 22:53:39 +0500

    Update the javascript part of the header section and add images

--- script/main.js
+++ script/main.js
@@ -1,13 +1,17 @@
 let headerOpenBtn = document.querySelector('.header-open--btn');
 let headerNavMobile = document.querySelector('.header-nav--mobile');
 let headerShade = document.querySelector('.header-shade');
-
+let headerNavMobileCloseBtn = document.querySelector('.header-nav-mobile--close-btn');
 
 headerOpenBtn.addEventListener('click', () => {
     headerNavMobile.style.transform = 'translateX(0%)';
     headerShade.style.transform = 'translateX(0%)';
 })
 headerShade.addEventListener('click', () => {
+    headerNavMobile.style.transform = 'translateX(-110%)';
+    headerShade.style.transform = 'translateX(-100%)';
+})
+headerNavMobileCloseBtn.addEventListener('click', () => {
     headerNavMobile.style.transform = 'translateX(-110%)';
     headerShade.style.transform = 'translateX(-100%)';
 })
