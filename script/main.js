9595aa2f7e53a7c8b93a994efcac96d69a4a358c
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-04-05 06:57:05 +0500

    Update the javascript part of the header section

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+let header = document.querySelector('.header-wrap-one');
 let headerOpenBtn = document.querySelector('.header-open--btn');
 let headerNavMobile = document.querySelector('.header-nav--mobile');
 let headerShade = document.querySelector('.header-shade');
@@ -15,3 +16,22 @@ headerNavMobileCloseBtn.addEventListener('click', () => {
     headerNavMobile.style.transform = 'translateX(-110%)';
     headerShade.style.transform = 'translateX(-100%)';
 })
+
+
+
+let currentScroll = 0;
+this.addEventListener('scroll', () => {
+
+    if(scrollY > currentScroll) {
+        header.classList.add('headScroll');
+    }
+    else if (scrollY == 0) {
+        header.style = 'box-shadow: none;'
+    }
+    else {
+        header.classList.remove('headScroll');
+        header.style = 'box-shadow: 0 2px 10px 0 rgba(0, 0, 0, .2);'
+    }
+
+    currentScroll = scrollY;
+}) 
